f3c9cc81e94d85a797e76fd49c518a4d946553ed
willyjo1 <arthurwilchield@gmail.com>
2025-08-03 21:33:51 +0000

    saved in the sub directory of the app

--- task1-image-grayscale/task1_grayscale.py
+++ task1-image-grayscale/task1_grayscale.py
@@ -1,6 +1,9 @@
 import cv2
 from matplotlib import pyplot as plt
 
+import os
+import cv2
+
 # Load image
 image = cv2.imread('images/photo.JPG')
 cv2.imshow('Original Image', image)
@@ -9,8 +12,16 @@ cv2.imshow('Original Image', image)
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Grayscale Image', gray_image)
 
+# Define subdirectory path
+output_dir = 'images/processed'
+output_path = os.path.join(output_dir, 'photo_gray.jpg')
+
+# Create the subdirectory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
+
 # Save grayscale image
-cv2.imwrite('../images/photo_gray.jpg', gray_image)
+cv2.imwrite(output_path, gray_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
